d7c52c2cbc98733ceb755555dece53838fb6a8dd
0-Xerife <bruno.barroso@polijunior.com.br>
2024-07-30 10:31:58 -0300

    :balloon: token input creation

---
+++ src/components/TokenInput/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: 'rgba(232, 232, 232, 0.7)',
+    backgroundColor: 'rgba(232, 232, 232, 0.7)',
+    borderRadius: 8,
+    margin: 15,
+    width: 45,
+    height: 45,
+    textAlign: 'center',
+    fontSize: 20,
+  },
+});
