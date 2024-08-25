985c82d05e8c87a5f8e13fb74f47d76fc696b85e
0-Xerife <bruno.barroso@polijunior.com.br>
2024-08-25 12:25:47 -0300

    :balloon: Screen creation

---
+++ src/components/AdressComponent/styles.ts
@@ -0,0 +1,54 @@
+import { theme } from '@styles/default.theme';
+import { View } from 'react-native';
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 95%;
+  justify-content: center;
+  padding: 0 16px;
+`;
+
+export const StyledInput = styled.TextInput`
+  font-family: 'Poppins-Regular';
+  font-size: 10px;
+  height: 40px;
+  background-color: ${theme.colors.primary.dark};
+  border-radius: 15px;
+  margin-top: 4px;
+  margin-bottom: 15px;
+  padding: 0 8px;
+`;
+
+export const StyledLabel = styled.Text`
+  /* margin-bottom: 2px; */
+  font-family: 'Poppins-Regular';
+  font-size: 10px;
+`;
+
+export const StyledButton = styled.TouchableOpacity`
+  background-color: ${theme.colors.primary.main};
+  padding: 10px;
+  border-radius: 30px;
+  align-items: center;
+  font-family: 'Poppins-Regular';
+  margin-top: 10px;
+`;
+
+export const ButtonText = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: white;
+  font-family: 'Poppins-Regular';
+  font-size: 16px;
+`;
+
+export const ThermsWrapper = styled(View)`
+  /* padding: 0px 30px; */
+  margin-top: 10px;
+  margin-bottom: 10px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  background: ${({ theme }) => theme.colors.white };
+`;
---
+++ src/screens/AdressSelection/index.tsx
@@ -0,0 +1,40 @@
+import * as S from './styles';
+import React, { useState } from 'react';
+import ProgressBar from '@components/ProgressBar';
+import Button from '@components/Button';
+import AdressComponent from '@components/AdressComponent';
+
+const AdressSelectionScreen = ({navigation}) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxToggle = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const handleNextPress = () => {
+    navigation.navigate('AdressSelection')
+  }
+
+  const handleBackPress = () => {
+    navigation.goBack();
+  };
+
+  return (
+
+  <S.Wrapper>
+    <ProgressBar 
+    currentPage={3} 
+    totalPages={5} 
+    onBackPress={handleBackPress} />
+    <S.TitleTxt>Qual é a sua idade?</S.TitleTxt>
+    <S.SubtitleTxt>Para uma melhor experiência com o app, por favor nos conte um 
+      pouco mais sobre você, seu endereço é essencial para te conhecermos melhor!</S.SubtitleTxt>
+    <AdressComponent />
+    <Button 
+    Title={'Próximo'} 
+    onPress={handleNextPress} />
+
+  </S.Wrapper>
+)};
+
+export default AdressSelectionScreen;
